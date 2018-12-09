97f9ec6171882ee65fe6a011af82b96cfb98cff0
Weiyi Wang <wwylele@gmail.com>
2018-12-08 19:36:28 -0500

    core_timing: add missing virtual dtor for callbacks

--- src/core_timing.h
+++ src/core_timing.h
@@ -13,6 +13,7 @@ class CoreTiming {
 public:
     class Callbacks {
     public:
+        virtual ~Callbacks() = default;
         virtual void Tick() = 0;
         virtual u64 GetMaxSkip() = 0;
         virtual void Skip(u64) = 0;
