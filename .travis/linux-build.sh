807671607f1bf3d2125620d954a14d06b2045757
liushuyu <liushuyu011@gmail.com>
2018-12-08 22:47:08 -0700

    travis: incorporating tests into CI

--- .travis/linux-build.sh
+++ .travis/linux-build.sh
@@ -7,6 +7,6 @@ export CC=gcc-7
 export CXX=g++-7
 
 mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DTEAKRA_TEST_ASSETS_DIR="$HOME/assets" -DTEAKRA_RUN_TESTS=ON
 cmake --build . -- -j"$(nproc)"
-ctest
+ctest -C Release
--- .travis/windows-build.sh
+++ .travis/windows-build.sh
@@ -5,6 +5,6 @@ set -x
 
 
 mkdir build && cd build
-cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release
+cmake .. -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DTEAKRA_TEST_ASSETS_DIR="$HOME/assets" -DTEAKRA_RUN_TESTS=OFF
 cmake --build . -- -maxcpucount
-ctest
+# ctest -C Release
