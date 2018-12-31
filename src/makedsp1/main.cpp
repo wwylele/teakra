9937f8fdc87fdf9eabcf74c6f37881462e13467f
Weiyi Wang <wwylele@gmail.com>
2018-12-31 07:15:49 -0500

    makedsp1: parse expansion smarter

--- src/makedsp1/main.cpp
+++ src/makedsp1/main.cpp
@@ -42,23 +42,37 @@ std::vector<std::string> StringToTokens(const std::string& in) {
 int main(int argc, char** argv) {
     auto parser = Teakra::GenerateParser();
 
-    if (argc < 3)
-        throw "nope";
+    if (argc < 3) {
+        printf("Not enough parameters\n");
+        return -1;
+    }
 
     std::ifstream in(argv[1]);
-    if (!in.is_open())
-        throw "fail";
+    if (!in.is_open()) {
+        printf("Failed to open input file\n");
+        return -1;
+    }
 
     std::string line;
     std::vector<Segment> segments;
-    u32 addr;
     int line_number = 0;
-    bool next_must_be_data = false;
     while (std::getline(in, line)) {
         ++line_number;
         auto comment_pos = line.find("//");
         if (comment_pos != std::string::npos) {
             line.erase(comment_pos);
+        }
+
+        auto expansion_pos = line.find("$");
+        bool has_expansion = expansion_pos != std::string::npos;
+        u16 expansion_data;
+        if (has_expansion) {
+            if (line.size() - expansion_pos < 5) {
+                printf("%d: unexpected line break in expansion data\n", line_number);
+                return -1;
+            }
+            expansion_data = (u16)std::stoi(line.substr(expansion_pos + 1, 4), 0, 16);
+            line = line.substr(0, expansion_pos) + "0000" + line.substr(expansion_pos + 5);
         }
 
         auto tokens = StringToTokens(line);
@@ -80,42 +94,48 @@ int main(int argc, char** argv) {
                 return -1;
             }
 
-            addr = s.target = std::stoi(tokens[2], 0, 16);
+            s.target = std::stoi(tokens[2], 0, 16);
             segments.push_back(s);
-            printf("New segment(%d) @ %04X\n", s.memory_type, s.target);
         } else {
             u16 v;
             if (tokens[0] == "data") {
-                next_must_be_data = false;
                 if (tokens.size() != 2) {
                     printf("%d: Wrong parameter count for 'data'\n", line_number);
                     return -1;
                 }
                 v = (u16)std::stoi(tokens[1], 0, 16);
+                segments.back().data.push_back(v);
             } else {
-                if (next_must_be_data) {
-                    printf("%d: must be data for expansion\n", line_number);
-                    return -1;
-                }
                 auto maybe_v = parser->Parse(tokens);
                 if (maybe_v.status == Teakra::Parser::Opcode::Invalid) {
                     printf("%d: could not parse\n", line_number);
                     return -1;
                 }
-                next_must_be_data = maybe_v.status == Teakra::Parser::Opcode::ValidWithExpansion;
                 v = maybe_v.opcode;
-            }
+                segments.back().data.push_back(v);
 
-            segments.back().data.push_back(v);
-            printf("%04X:%04X\n", addr, v);
-            ++addr;
+                if (maybe_v.status == Teakra::Parser::Opcode::ValidWithExpansion) {
+                    if (!has_expansion) {
+                        printf("%d: needs expansion\n", line_number);
+                        return -1;
+                    }
+                    segments.back().data.push_back(expansion_data);
+                } else {
+                    if (has_expansion) {
+                        printf("%d: unexpected expansion\n", line_number);
+                        return -1;
+                    }
+                }
+            }
         }
     }
     in.close();
 
     FILE* out = fopen(argv[2], "wb");
-    if (!out)
-        throw "fail!!!";
+    if (!out) {
+        printf("Failed to open output file\n");
+        return -1;
+    }
 
     u32 data_ptr = 0x300;
 
